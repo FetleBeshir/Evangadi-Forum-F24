3f87b18423cc7c8b91500f92b244313cb4bd81e8
FetleBeshir <fetlebeshir5@gmail.com>
2024-08-15 21:04:01 -0700

    Evangadi Forum Final Deploy

--- backend/app.js
+++ backend/app.js
@@ -12,12 +12,10 @@ const answerRoutes = require("./routes/answerRoute");
 const questionRoutes = require("./routes/questionRoute");
 
 // JSON middleware to extract JSON data
-app.use(express.json());
-const corsOptions = {
-  origin: "http://localhost:5173", // Replace with the allowed origin
-};
 
-app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use(cors());
 
 
 //user routes middleware file
