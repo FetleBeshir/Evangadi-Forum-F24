d0f609fc007030e3bd8604c2e5aa1e75cdbbbbb8
FetleBeshir <fetlebeshir5@gmail.com>
2024-08-15 20:58:36 -0700

    Evangadi Forum Final Deploy

--- client/src/API/axios.js
+++ client/src/API/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create an instance of axios
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:7500", // Update with your base URL
+  baseURL: "https://evangadi-forum-f24.onrender.com", // Update with your base URL
 });
 
 // Request interceptor to include token in headers
